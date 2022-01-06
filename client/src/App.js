80c5e6bff93af131c60658085ef9e190e6a9b678
Zucely Hernandez <andreazucely70@gmail.com>
2022-01-05 18:59:32 -0600

    sección 7

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React, {Fragment} from 'react';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+//Redux
+import { Provider } from 'react-redux';
+import store from './store';
+
+import './App.css';
+
+const App = () => (
+  <Provider store={store}>
+    <Router>
+      <Fragment>
+        {/*página principal de login*/}
+        <Navbar />
+         <Routes>
+          <Route path='/' element={<Landing/>}/>
+         </Routes>
+         <section className='container'>
+           <Routes> {/*switch*/}
+            <Route path='/register' element={<Register/>}/>
+            <Route path='/login' element={<Login/>}/>
+           </Routes> 
+         </section>
+      </Fragment>
+    </Router>
+    </Provider>
+);
+
+export default App; 
---
+++ client/src/reducers/alert.js
@@ -0,0 +1,25 @@
+import {SET_ALERT, REMOVE_ALERT} from '../actions/types' 
+//reducer
+
+// alert
+const initialState = [
+   /*  {   
+        id: 1,
+        msg: 'Please Log in',
+        alertType: 'success'
+    } */
+];
+
+export default function(state = initialState, action){
+    const {type, payload} = action;
+
+    switch(type){
+        case SET_ALERT:
+            return [...state, payload];
+        case REMOVE_ALERT:
+            //remove by id
+            return state.filter(alert => alert.id !== payload); //payload in this case is ID 
+        default:
+            return state;
+    }
+}
